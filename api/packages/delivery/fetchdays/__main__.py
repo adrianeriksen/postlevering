345b4217726e4b71451427488337050a47a73d7a
Adrian Alexander Eriksen <adrianalexeriksen@gmail.com>
2023-01-25 14:11:26 +0100

    Restructure project with client

--- api/packages/delivery/fetchdays/__main__.py
+++ api/packages/delivery/fetchdays/__main__.py
@@ -4,7 +4,7 @@ from http import HTTPStatus
 from posten_service import get_next_delivery_days, is_post_code_valid
 
 def main(args):
-    post_code = args.get("post-code")
+    post_code = args.get("postcode")
 
     if not is_post_code_valid(post_code):
         return { "statusCode": HTTPStatus.BAD_REQUEST }
