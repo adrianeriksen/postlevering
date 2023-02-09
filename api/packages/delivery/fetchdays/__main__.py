aad25ba2e65d1baad35b77664640f6b5a5d2c9f4
Adrian Alexander Eriksen <adrianalexeriksen@gmail.com>
2023-02-09 14:59:31 +0100

    Fix reference to nonexisting variable

--- api/packages/delivery/fetchdays/__main__.py
+++ api/packages/delivery/fetchdays/__main__.py
@@ -6,7 +6,7 @@ from posten_service import get_next_delivery_days, is_post_code_valid
 def main(args):
     postcode = args.get("postcode")
 
-    if not is_post_code_valid(post_code):
+    if not is_post_code_valid(postcode):
         return { "statusCode": HTTPStatus.BAD_REQUEST }
 
     try:
